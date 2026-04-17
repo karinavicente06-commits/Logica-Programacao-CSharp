a293209a695df98aa0d0acf2e7de47b4d5982a44
Lucas Baer <lucasbaer2018@gmail.com>
2026-04-16 21:23:17 -0300

    Busca simples de cadastro

--- GitBranch/Program.cs
+++ GitBranch/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace GitBranch
+﻿string[] nomes = new string[5];
+int qtdCadastros = 0;
+bool continuar = true;
+
+Console.Write("Digite um nome para busca: ");
+string busca = Console.ReadLine().ToUpper();
+
+if (nomes.Contains(busca))
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    Console.WriteLine("Nome informado encontrado! ");
+}
+else
+{
+    Console.WriteLine("Nome informado não encontrado!");
 }
